b11de0f2334bcb141dd62dadefd4cf3b6964f16c
Firox <nicolas.indjein@epita.fr>
2023-02-07 23:49:18 +0100

    feat(buffers): vertex data buffers and layouts

---
+++ src/core/VertexArray.cpp
@@ -0,0 +1,42 @@
+#include "VertexArray.h"
+
+#include <GL/glew.h>
+
+#include "glErrors.h"
+
+namespace BerylEngine
+{
+	VertexArray::VertexArray(const ByteBuffer& vb, const VertexBufferLayout& layout)
+	{
+		GL_CALL(glGenVertexArrays(1, &m_id));
+		GL_CALL(glBindVertexArray(m_id));
+
+		vb.bind(BufferUsageType::VertexBuffer);
+		const auto& elements = layout.get_elements();
+		size_t offset = 0;
+		for (unsigned int i = 0; i < elements.size(); i++)
+		{
+			const auto elm = elements[i];
+			GL_CALL(glVertexAttribPointer(i, elm.count, elm.type,
+				elm.normalized, layout.get_stride(), (const void*)offset));
+			GL_CALL(glEnableVertexAttribArray(i));
+
+			offset += elm.count * VertexBufferElement::get_type_size(elm.type);
+		}
+	}
+
+	VertexArray::~VertexArray()
+	{
+		GL_CALL(glDeleteVertexArrays(1, &m_id));
+	}
+
+	void VertexArray::bind() const
+	{
+		GL_CALL(glBindVertexArray(m_id));
+	}
+
+	void VertexArray::unbind() const
+	{
+		GL_CALL(glBindVertexArray(0));
+	}
+}
---
+++ src/core/VertexArray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ByteBuffer.h"
+#include "VertexBufferLayout.h"
+
+namespace BerylEngine
+{
+	class VertexArray
+	{
+	private:
+		unsigned int m_id;
+
+	public:
+		VertexArray(const ByteBuffer& vb, const VertexBufferLayout& layout);
+		VertexArray(const VertexArray&) = delete;
+		~VertexArray();
+
+		void bind() const;
+		void unbind() const;
+	};
+}
