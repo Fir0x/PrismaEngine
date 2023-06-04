ec25796fbdc59dfb918dae18ccc9082263e23231
nicolas.indjein <nicolas.indjein@epita.fr>
2023-06-04 23:20:42 +0200

    feat(maths): angle conversion utils implementation

--- src/core/maths/public/utils.h
+++ src/core/maths/public/utils.h
@@ -1,11 +1,30 @@
 #pragma once
 
-#include <glm/glm.hpp>
+#define PRISMA_PI 3.14159265358979323846
 
 namespace PrismaEngine
 {
 	inline float degreesToRadians(float x)
 	{
-		return glm::radians(x);
+		constexpr float ratio = static_cast<float>(PRISMA_PI) / 180.0f;
+		return x * ratio;
+	}
+
+	inline double degreesToRadians(double x)
+	{
+		constexpr double ratio = static_cast<double>(PRISMA_PI) / 180.0;
+		return x * ratio;
+	}
+
+	inline float radiansToDegrees(float x)
+	{
+		constexpr float ratio = 180.0f / static_cast<float>(PRISMA_PI);
+		return x * ratio;
+	}
+
+	inline double radiansToDegrees(double x)
+	{
+		constexpr double ratio = 180.0f / static_cast<double>(PRISMA_PI);
+		return x * ratio;
 	}
 }
